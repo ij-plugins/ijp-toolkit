ce0f52bff9dd4446c8bd24844daa3b465f765b17
Jarek Sacha <jpsacha@gmail.com>
2002-10-09 02:42:34 +0000

    Initial.

---
+++ Toolkit/src/net/sf/ij/im3d/grow/ConnectedThresholdFilterUInt16.java
@@ -0,0 +1,83 @@
+/***
+ * Image/J Plugins
+ * Copyright (C) 2002 Jarek Sacha
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Latest release available at http://sourceforge.net/projects/ij-plugins/
+ */
+package net.sf.ij.im3d.grow;
+
+import ij.*;
+
+import java.util.*;
+
+import net.sf.ij.im3d.*;
+
+/**
+ *  Simple region growing algorithm that extracts all pixels connected to the
+ *  seed as long as they intensities are within given threshold limits. Min
+ *  limit is inclusive, max limit is exclusive.
+ *
+ *@author     Jarek Sacha
+ *@created    April 29, 2002
+ *@version    $Revision: 1.1 $
+ */
+
+public class ConnectedThresholdFilterUInt16 extends ConnectedThresholdFilterBase {
+
+  /** Source pixels */
+  protected short[][] srcPixels = null;
+
+
+  /*
+   *
+   */
+  final protected void createHandleToSrcPixels(ImageStack src) {
+    Object[] imageArray = src.getImageArray();
+
+    int n = src.getSize();
+    srcPixels = new short[n][];
+    for (int z = 0; z < n; ++z) {
+      srcPixels[z] = (short[]) imageArray[z];
+      if (!(imageArray[z] instanceof short[])) {
+        throw new IllegalArgumentException("Expecting stack of byte images.");
+      }
+    }
+  }
+
+
+  /*
+   *
+   */
+  final protected void checkForGrow(int x, int y, int z) {
+    if (x < xMin || x >= xMax ||
+        y < yMin || y >= yMax ||
+        z < zMin || z >= zMax) {
+      return;
+    }
+
+    int offset = y * xSize + x;
+    if (destPixels[z][offset] == BACKGROUND) {
+      int value = srcPixels[z][offset] & 0xffff;
+      if (value >= valueMin && value < valueMax) {
+        destPixels[z][offset] = MARKER;
+        candidatePoints.addLast(new Point3D(x, y, z));
+      } else {
+        destPixels[z][offset] = NOT_MEMBER;
+      }
+    }
+  }
+}
