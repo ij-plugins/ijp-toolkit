abc218c1bad8fa6eeecb7bc2db0aecbf8e60eab1
Jarek Sacha <jpsacha@gmail.com>
2002-10-09 02:43:08 +0000

    Support 16 bit images.

--- Plugins/3D Toolkit/src/3D Toolkit/Connected_Threshold_Grower.java
+++ Plugins/3D Toolkit/src/3D Toolkit/Connected_Threshold_Grower.java
@@ -1,22 +1,15 @@
-/***
- * Image/J Plugins
- * Copyright (C) 2002 Jarek Sacha
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Latest release available at http://sourceforge.net/projects/ij-plugins/
+/**
+ *  Image/J Plugins Copyright (C) 2002 Jarek Sacha This library is free
+ *  software; you can redistribute it and/or modify it under the terms of the
+ *  GNU Lesser General Public License as published by the Free Software
+ *  Foundation; either version 2.1 of the License, or (at your option) any later
+ *  version. This library is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ *  License for more details. You should have received a copy of the GNU Lesser
+ *  General Public License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ *  USA Latest release available at http://sourceforge.net/projects/ij-plugins/
  */
 import ij.*;
 import ij.gui.*;
@@ -32,51 +25,68 @@ import net.sf.ij.im3d.grow.*;
  *
  * @author     Jarek Sacha
  * @created    July 14, 2002
- * @version    $Revision: 1.2 $
+ * @version    $Revision: 1.3 $
  */
 
 public class Connected_Threshold_Grower implements PlugIn {
 
-  /**
-   *  Main processing method for the VTK_Writer plugin
-   *
-   * @param  arg  Optional argument required by ij.plugin.PlugIn interface (not
-   *      used).
-   */
-  public void run(String arg) {
-    ImagePlus imp = WindowManager.getCurrentImage();
-    if (imp == null) {
-      IJ.noImage();
-      return;
+    private static Point3D seed = new Point3D();
+    private static int valueMin = 0;
+    private static int valueMax = 128;
+
+
+    /**
+     *  Main processing method for the VTK_Writer plugin
+     *
+     * @param  arg  Optional argument required by ij.plugin.PlugIn interface
+     *      (not used).
+     */
+    public void run(String arg) {
+        ImagePlus imp = WindowManager.getCurrentImage();
+        if (imp == null) {
+            IJ.noImage();
+            return;
+        }
+
+        // TODO: use ConnectedThresholdFilter factory to create approptiate filter type.
+        ConnectedThresholdFilterBase connectedThresholdFilter = null;
+        if (imp.getType() == ImagePlus.GRAY8) {
+            connectedThresholdFilter = new ConnectedThresholdFilterUInt8();
+        }
+        else if (imp.getType() == ImagePlus.GRAY16) {
+            connectedThresholdFilter = new ConnectedThresholdFilterUInt16();
+        }
+        else {
+            IJ.showMessage("ERROR", "Unsupported image type. " +
+                    " Supporter types are GRAY8 and GRAY16.");
+            return;
+        }
+
+        GenericDialog gd = new GenericDialog("Grow options");
+        gd.addMessage("Seed point coordinates");
+        gd.addNumericField("x", seed.x, 0);
+        gd.addNumericField("y", seed.y, 0);
+        gd.addNumericField("z", seed.z, 0);
+        gd.addMessage("Threshold limits");
+        gd.addNumericField("min", valueMin, 0);
+        gd.addNumericField("max", valueMax, 0);
+
+        gd.showDialog();
+
+        if (gd.wasCanceled()) {
+            return;
+        }
+
+        seed.x = (int)gd.getNextNumber();
+        seed.y = (int)gd.getNextNumber();
+        seed.z = (int)gd.getNextNumber();
+        valueMin = (int)gd.getNextNumber();
+        valueMax = (int)gd.getNextNumber();
+
+        connectedThresholdFilter.setValueMin(valueMin);
+        connectedThresholdFilter.setValueMax(valueMax);
+        ImageStack out = connectedThresholdFilter.run(imp.getStack(), seed);
+
+        new ImagePlus("Connected region", out).show();
     }
-
-    GenericDialog gd = new GenericDialog("Grow options");
-    gd.addMessage("Seed point coordinates");
-    gd.addNumericField("x", 0, 0);
-    gd.addNumericField("y", 0, 0);
-    gd.addNumericField("z", 0, 0);
-    gd.addMessage("Threshold limits");
-    gd.addNumericField("min", 0, 0);
-    gd.addNumericField("max", 0, 0);
-
-    gd.showDialog();
-
-    if (gd.wasCanceled()) {
-      return;
-    }
-
-    Point3D seed = new Point3D();
-    seed.x = (int) gd.getNextNumber();
-    seed.y = (int) gd.getNextNumber();
-    seed.z = (int) gd.getNextNumber();
-    int valueMin = (int) gd.getNextNumber();
-    int valueMax = (int) gd.getNextNumber();
-
-    ConnectedThresholdFilter ctf = new ConnectedThresholdFilter();
-    ctf.setValueMin(valueMin);
-    ctf.setValueMax(valueMax);
-    ImageStack out = ctf.run(imp.getStack(), seed);
-
-    new ImagePlus("Region", out).show();
-  }
 }
