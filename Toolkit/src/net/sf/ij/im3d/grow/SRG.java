4481cea7a1048adfaf1cc1f507107affa5b92975
Jarek Sacha <jpsacha@gmail.com>
2003-12-23 03:03:08 +0000

    Update header comment.

--- Toolkit/src/net/sf/ij/im3d/grow/SRG.java
+++ Toolkit/src/net/sf/ij/im3d/grow/SRG.java
@@ -1,6 +1,6 @@
 /***
  * Image/J Plugins
- * Copyright (C) 2002 Jarek Sacha
+ * Copyright (C) 2002,2003 Jarek Sacha
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
