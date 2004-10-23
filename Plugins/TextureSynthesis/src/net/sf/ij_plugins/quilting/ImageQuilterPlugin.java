442fcbbdcf480bd729805855f48ad50564d8b571
Jarek Sacha <jpsacha@gmail.com>
2004-10-23 03:26:47 +0000

    no message

--- Plugins/TextureSynthesis/src/net/sf/ij_plugins/quilting/ImageQuilterPlugin.java
+++ Plugins/TextureSynthesis/src/net/sf/ij_plugins/quilting/ImageQuilterPlugin.java
@@ -39,18 +39,23 @@ public class ImageQuilterPlugin implements PlugInFilter {
     final private static String PLUGIN_NAME = "Image Quilter";
     final private static String ABOUT_COMMAND = "about";
     final private static String ABOUT_MESSAGE =
-            "Performs texture synthesis using image quilting algorithms of Efros and Freeman.\n" +
+            "Image Quilter plugin performs texture synthesis using image quilting\n"
+            + "algorithms of Efros and Freeman:\n" +
+            "http://www.cs.berkeley.edu/~efros/research/quilting.html\n" +
+            "  \n" +
             "Parameters:\n" +
-            "   " + DEST_WIDTH_LABEL + " - desired width of the output image.\n" +
-            "   " + DEST_HEIGHT_LABEL + " - desired height of the output image.\n" +
+            "   " + DEST_WIDTH_LABEL + " - desired width of the output image, actual width be " +
+            "       slightly larger, depending on patch size.\n" +
+            "   " + DEST_HEIGHT_LABEL + " - desired height of the output image, actual height be \n" +
+            "       slightly larger, depending on patch size.\n" +
             "   " + PATCH_SIZE_LABEL + " - width and height of a patch used for quilting.\n" +
             "   " + PATCH_OVERLAP_LABEL + " - amount of overlap between patches when matching.\n" +
             "   " + ENABLE_HORIZ_PATHS_LABEL + " - enable improved matching by weighted paths.\n" +
             "   " + PATCH_COST_WEIGHT_LABEL + " - weight used for improved matching." +
-            "\n" +
+            "  \n" +
             "The code of this plugin was originally developed by by Nick Vavra.\n" +
             "Original code and description is available at: http://www.cs.wisc.edu/~vavra/cs766/.\n" +
-            "ImageJ port info available at: http://ij-plugins.sf.net/plugins/texturesynthesis/.";
+            "ImageJ port and info is available at: http://ij-plugins.sf.net/plugins/texturesynthesis/.";
 
     private static Config config = new Config();
 
