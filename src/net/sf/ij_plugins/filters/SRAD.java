64203e99019deb23679663149c2c0b07aefea10e
Jarek Sacha <jpsacha@gmail.com>
2005-10-10 04:03:34 +0000

    Correct mistake in implementation of Equation (57). Optimize computation of q0t and q0t2.

--- src/net/sf/ij_plugins/filters/SRAD.java
+++ src/net/sf/ij_plugins/filters/SRAD.java
@@ -20,6 +20,7 @@
  */
 package net.sf.ij_plugins.filters;
 
+import ij.IJ;
 import ij.process.FloatProcessor;
 import net.sf.ij_plugins.operators.Neighborhood3x3;
 import net.sf.ij_plugins.operators.PixelIterator;
@@ -85,6 +86,9 @@ public class SRAD extends AbstractAnisotropicDiffusion {
         final PixelIterator iterator = new PixelIterator(src);
 
         // Precalculate diffusion coefficient c(q)
+        final double q0t = q0 * Math.exp(-ro * time());
+        final double q0t2 = q0t * q0t;
+        IJ.log("time=" + time() + ", q0=" + q0 + ", q0t=" + q0t);
         while (iterator.hasNext()) {
             final Neighborhood3x3 n = (Neighborhood3x3) iterator.next();
 
@@ -100,15 +104,13 @@ public class SRAD extends AbstractAnisotropicDiffusion {
             final double dLI1 = (n.center - n.neighbor5);
             final double dLI2 = (n.center - n.neighbor3);
             // Equation (54), h=1
-            final double d2I = (n.neighbor1 + n.neighbor5 + n.neighbor7 + n.neighbor3);
+            final double d2I = (n.neighbor1 + n.neighbor5 + n.neighbor7 + n.neighbor3 - 4 * n.center);
 
             // Equation (57)
             final double v1 = Math.sqrt(dRI1 * dRI1 + dRI2 + dRI2 + dLI1 * dLI1 + dLI2 * dLI2) / n.center;
             final double v2 = d2I / n.center;
             final double v3 = 1 + v2 / 4;
             final double q = Math.sqrt((v1 * v1 / 2 - v2 * v2 / 16) / (v3 * v3));
-            final double q0t = q0 * Math.exp(-ro * time());
-            final double q0t2 = q0t * q0t;
             // Equation (33)
             final double cij = 1 / (1 + (q * q - q0t2) / (q0t2 * (1 + q0t2)));
 
@@ -136,7 +138,7 @@ public class SRAD extends AbstractAnisotropicDiffusion {
                     cij1 * (n.neighbor7 - n.center) + cij * (n.neighbor3 - n.center);
 
             // SRAD update, equation (61)
-            final double newValue = n.center + (getTimeStep() / 4 * d);
+            final double newValue = n.center + (getTimeStep() / 4d * d);
             destPixels[n.offset] = (float) newValue;
         }
     }
