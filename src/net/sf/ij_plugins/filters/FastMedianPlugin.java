ee8ee9baeba9d705509e2cb1c35459535083dc99
Jarek Sacha <jpsacha@gmail.com>
2006-11-21 02:51:22 +0000

    Add support for median filering of RGB images.

--- src/net/sf/ij_plugins/filters/FastMedianPlugin.java
+++ src/net/sf/ij_plugins/filters/FastMedianPlugin.java
@@ -1,6 +1,6 @@
-/***
+/*
  * Image/J Plugins
- * Copyright (C) 2002-2004 Jarek Sacha
+ * Copyright (C) 2002-2006 Jarek Sacha
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,6 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Latest release available at http://sourceforge.net/projects/ij-plugins/
+ *
  */
 package net.sf.ij_plugins.filters;
 
@@ -25,8 +26,10 @@ import ij.ImagePlus;
 import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ByteProcessor;
+import ij.process.ColorProcessor;
 import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
+import net.sf.ij_plugins.color.ColorProcessorUtils;
 
 /**
  * @author Jarek Sacha
@@ -35,7 +38,7 @@ import ij.process.ImageProcessor;
 
 public class FastMedianPlugin implements PlugInFilter {
     public int setup(String s, ImagePlus imagePlus) {
-        return DOES_8G | DOES_16 | DOES_32 | NO_CHANGES;
+        return DOES_8G | DOES_16 | DOES_32 | DOES_RGB | NO_CHANGES;
     }
 
     public void run(final ImageProcessor ip) {
@@ -50,37 +53,23 @@ public class FastMedianPlugin implements PlugInFilter {
 
         int filterSize = (int) Math.round(genericDialog.getNextNumber());
 
+        final ImageProcessor dest;
+        final long start = System.currentTimeMillis();
         if (ip instanceof ByteProcessor) {
-            final ByteProcessor src = (ByteProcessor) ip;
-
-
-            final FastMedianUInt8 filter = new FastMedianUInt8();
-
-            // Set progress bar
-            if (IJ.getInstance() != null) {
-                filter.setProgressBar(IJ.getInstance().getProgressBar());
+            dest = run((ByteProcessor) ip, filterSize);
+        } else if (ip instanceof ColorProcessor) {
+            final ByteProcessor[] srcBps = ColorProcessorUtils.splitRGB((ColorProcessor) ip);
+            for (int i = 0; i < srcBps.length; i++) {
+                srcBps[i] = run(srcBps[i], filterSize);
             }
-
-            // Perform filtering
-            long start = System.currentTimeMillis();
-            ByteProcessor dest = filter.run(src, filterSize, filterSize);
-            long end = System.currentTimeMillis();
-
-            // Show results
-            new ImagePlus("Median", dest).show();
-
-            if (IJ.debugMode) {
-                IJ.write("Median filtering completed in " + (end - start) + "ms.");
-            }
+            dest = ColorProcessorUtils.mergeRGB(srcBps);
         } else {
             final FloatProcessor src = (FloatProcessor) (ip instanceof FloatProcessor
                     ? ip
                     : ip.convertToFloat());
 
-
             final RunningFilter filter
                     = new RunningFilter(new RunningMedianOperator(), filterSize, filterSize);
-
 
             // Set progress bar
             if (IJ.getInstance() != null) {
@@ -88,16 +77,25 @@ public class FastMedianPlugin implements PlugInFilter {
             }
 
             // Perform filtering
-            long start = System.currentTimeMillis();
-            FloatProcessor dest = filter.run(src);
-            long end = System.currentTimeMillis();
-
-            // Show results
-            new ImagePlus("Median", dest).show();
-
-            if (IJ.debugMode) {
-                IJ.write("Median filtering completed in " + (end - start) + "ms.");
-            }
+            dest = filter.run(src);
         }
+        final long end = System.currentTimeMillis();
+
+        // Show results
+        new ImagePlus("Median", dest).show();
+
+        if (IJ.debugMode) {
+            IJ.write("Median filtering completed in " + (end - start) + "ms.");
+        }
+    }
+
+    private static ByteProcessor run(ByteProcessor src, int filterSize) {
+        final FastMedianUInt8 filter = new FastMedianUInt8();
+
+        // Set progress bar
+        if (IJ.getInstance() != null) {
+            filter.setProgressBar(IJ.getInstance().getProgressBar());
+        }
+        return filter.run(src, filterSize, filterSize);
     }
 }
