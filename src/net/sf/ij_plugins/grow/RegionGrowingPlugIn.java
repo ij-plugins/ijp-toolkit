33e563717ff76536b3b0a0cd4d87e90a8b93671b
Jarek Sacha <jpsacha@gmail.com>
2009-10-16 02:29:24 +0000

    1) Add support for processing slices. 2) Copy LUT from seeds to segmented image.

--- src/net/sf/ij_plugins/grow/RegionGrowingPlugIn.java
+++ src/net/sf/ij_plugins/grow/RegionGrowingPlugIn.java
@@ -22,17 +22,17 @@
 
 package net.sf.ij_plugins.grow;
 
-import ij.IJ;
-import ij.ImagePlus;
-import ij.ImageStack;
-import ij.WindowManager;
+import ij.*;
 import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 import ij.process.ByteProcessor;
+import ij.process.ImageProcessor;
 import net.sf.ij_plugins.im3d.grow.SRG;
+import net.sf.ij_plugins.util.progress.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 
 /**
@@ -44,78 +44,190 @@ import java.util.List;
 public final class RegionGrowingPlugIn implements PlugIn {
 
     private static final String TITLE = "Seeded Region Growing";
-    private static final String[] STACK_TREATMENT = {"Independent slices", "3D Volume", "Multi-band"};
+    private static final String RUN_CURRENT_SLICE = "Current slice only";
+    private static final String RUN_INDEPENDENT_SLICES = "All slices independent";
+    private static final String RUN_3D = "3D volume";
+    private static final String[] STACK_TREATMENT = {RUN_CURRENT_SLICE, RUN_INDEPENDENT_SLICES, RUN_3D};
+
 
     public void run(final String arg) {
+
         final int[] wList = WindowManager.getIDList();
         if (wList == null) {
             IJ.noImage();
             return;
         }
 
-
-        final List<String> titleList = new ArrayList<String>();
+        final List<String> imageTitleList = new ArrayList<String>();
+        final List<String> seedTitleList = new ArrayList<String>();
         for (final int id : wList) {
             final ImagePlus imp = WindowManager.getImage(id);
             if (imp != null && !imp.getTitle().trim().isEmpty()) {
-                titleList.add(imp.getTitle());
+                if (isSupportedImage(imp)) {
+                    imageTitleList.add(imp.getTitle());
+                }
+                if (isSupportedSeed(imp)) {
+                    seedTitleList.add(imp.getTitle());
+                }
             }
         }
 
-        final String[] titles = titleList.toArray(new String[titleList.size()]);
+        if (imageTitleList.size() < 1) {
+            IJ.error(TITLE, "No supported images open.");
+            return;
+        }
+        if (seedTitleList.size() < 1) {
+            IJ.error(TITLE, "No supported seed images open.");
+            return;
+        }
+
+        final String[] imageTitles = imageTitleList.toArray(new String[imageTitleList.size()]);
+        final String[] seedTitles = seedTitleList.toArray(new String[seedTitleList.size()]);
         final GenericDialog gd = new GenericDialog(TITLE, IJ.getInstance());
-        gd.addChoice("Image:", titles, titles[0]);
-        gd.addChoice("Seeds:", titles, titles[1]);
-//        gd.addChoice("Stack treatment:", STACK_TREATMENT, STACK_TREATMENT[0]);
+        gd.addChoice("Image:", imageTitles, imageTitles[0]);
+        gd.addChoice("Seeds:", seedTitles, seedTitles[0]);
+        gd.addChoice("Stack treatment:", STACK_TREATMENT, STACK_TREATMENT[0]);
         gd.addMessage("Seeds image should be of the same size as the image for segmentation.");
         gd.showDialog();
         if (gd.wasCanceled()) {
             return;
         }
 
-        final ImagePlus image = WindowManager.getImage(wList[gd.getNextChoiceIndex()]);
-        final ImagePlus seeds = WindowManager.getImage(wList[gd.getNextChoiceIndex()]);
-//        final String stackTreatment = gd.getNextString();
+        final ImagePlus image = WindowManager.getImage(imageTitles[gd.getNextChoiceIndex()]);
+        final ImagePlus seeds = WindowManager.getImage(seedTitles[gd.getNextChoiceIndex()]);
+        final String stackTreatment = gd.getNextChoice();
 
-//        run(image, seeds, stackTreatment.trim());
-        run(image, seeds, STACK_TREATMENT[0]);
-    }
-
-
-    void run(final ImagePlus image, final ImagePlus seeds, final String stackTreatment) {
-
-        if (!STACK_TREATMENT[0].equalsIgnoreCase(stackTreatment)) {
-            for (int i = 1; i < STACK_TREATMENT.length; ++i) {
-                if (STACK_TREATMENT[i].equalsIgnoreCase(stackTreatment)) {
-                    IJ.error(TITLE, "Not yet implemented");
-                    return;
-                }
-            }
-            IJ.error(TITLE, "Not supported.");
+        // Validate image types
+        if (!isSupportedImage(image)) {
+            IJ.error(TITLE, "Unsupported image type. Must be GRAY8, GRAY16 or GRAY32.");
+            return;
+        }
+        if (!isSupportedSeed(seeds)) {
+            IJ.error(TITLE, "Unsupported seed image type. Must be GRAY8 or COLOR_256 (indexed color).");
             return;
         }
 
+        // Run SRG
+        run(image, seeds, stackTreatment);
+    }
 
-        // FIXME: validate size and type
+
+    private boolean isSupportedImage(final ImagePlus imp) {
+        final int type = imp.getType();
+        return type == ImagePlus.GRAY8 || type == ImagePlus.GRAY16 || type == ImagePlus.GRAY32;
+    }
+
+
+    private boolean isSupportedSeed(final ImagePlus imp) {
+        final int type = imp.getType();
+        return type == ImagePlus.GRAY8 || type == ImagePlus.COLOR_256;
+    }
+
+
+    private void run(final ImagePlus image, final ImagePlus seeds, final String stackTreatment) {
+        if (RUN_CURRENT_SLICE.equalsIgnoreCase(stackTreatment)) {
+            run(image.getProcessor(), (ByteProcessor) seeds.getProcessor(), image.getTitle() + Integer.toString(image.getCurrentSlice()));
+        } else if (RUN_INDEPENDENT_SLICES.equalsIgnoreCase(stackTreatment)) {
+            runEachSliceIndependently(image, seeds);
+        } else if (RUN_3D.equalsIgnoreCase(stackTreatment)) {
+            IJ.error(TITLE, "Not yet implemented stack option: " + stackTreatment);
+        } else {
+            IJ.error(TITLE, "Not supported stack option: " + stackTreatment);
+        }
+    }
+
+
+    private void run(final ImageProcessor image, final ByteProcessor seeds, final String prefix) {
+        final SRG srg = new SRG();
+        srg.setImage(image);
+        srg.setSeeds(seeds);
+
+        // Forward progress notification
+        srg.addProgressListener(new IJProgressBarAdapter());
+
+        // Run segmentation
+        srg.run();
+
+        final ByteProcessor r = srg.getRegionMarkers();
+        r.setColorModel(seeds.getColorModel());
+        new ImagePlus(prefix + "-SRG", r).show();
+    }
+
+
+    private void runEachSliceIndependently(final ImagePlus image, final ImagePlus seeds) {
+
+        // Validate size
+        if (image.getWidth() != seeds.getWidth() || image.getHeight() != seeds.getHeight() || image.getStackSize() != seeds.getStackSize()) {
+            IJ.error(TITLE, "Size of the image [" + image.getWidth() + "x" + image.getHeight() + "x" + image.getStackSize() + "] " +
+                    "must match size of seeds [" + seeds.getWidth() + "x" + seeds.getHeight() + "x" + seeds.getStackSize() + "].");
+            return;
+        }
+
+        final ExecutorService threadPool = Executors.newFixedThreadPool(Prefs.getThreads());
 
         // Process
+        final IJProgressBarAdapter progressBarAdapter = new IJProgressBarAdapter();
+        final ProgressAccumulator accumulator = new ProgressAccumulator();
+        accumulator.addProgressListener(progressBarAdapter);
         final ImageStack stack = new ImageStack(image.getWidth(), image.getHeight());
-        for (int i = 1; i <= image.getNSlices(); ++i) {
-            final ByteProcessor bp = (ByteProcessor) image.getStack().getProcessor(i);
-            final ByteProcessor s = (ByteProcessor) seeds.getStack().getProcessor(i);
-            final ByteProcessor destBP = run(bp, s);
-            stack.addSlice(image.getStack().getSliceLabel(i), destBP);
+        final List<Future<ByteProcessor>> futures = new ArrayList<Future<ByteProcessor>>(image.getNSlices());
+        for (int i = 0; i < image.getNSlices(); ++i) {
+            final ByteProcessor bp = (ByteProcessor) image.getStack().getProcessor(i + 1);
+            final ByteProcessor s = (ByteProcessor) seeds.getStack().getProcessor(i + 1);
+            final SRGCallable worker = new SRGCallable(bp, s);
+
+            accumulator.addProgressReporter(worker);
+            final Future<ByteProcessor> f = threadPool.submit(worker);
+            futures.add(f);
+        }
+
+
+        // Wait for all futures to complete using Future.get()
+        for (int i = 0; i < futures.size(); i++) {
+            final Future<ByteProcessor> worker = futures.get(i);
+            final String sliceLabel = image.getStack().getSliceLabel(i + 1);
+            try {
+                stack.addSlice(sliceLabel, worker.get());
+            } catch (final InterruptedException e) {
+                e.printStackTrace();
+            } catch (final ExecutionException e) {
+                e.printStackTrace();
+            }
         }
 
         new ImagePlus(image.getTitle() + "-SRG", stack).show();
     }
 
-    final ByteProcessor run(final ByteProcessor image, final ByteProcessor seeds) {
-        final SRG srg = new SRG();
-        srg.setImage(image);
-        srg.setSeeds(seeds);
-        srg.run();
-        return srg.getRegionMarkers();
-    }
 
+    private static class SRGCallable extends DefaultProgressReporter implements Callable<ByteProcessor> {
+
+        final ByteProcessor image;
+        final ByteProcessor seeds;
+
+
+        private SRGCallable(final ByteProcessor image, final ByteProcessor seeds) {
+            this.image = image;
+            this.seeds = seeds;
+        }
+
+
+        public ByteProcessor call() throws Exception {
+            // Setup SRG
+            final SRG srg = new SRG();
+            srg.setImage(image);
+            srg.setSeeds(seeds);
+            // Forward progress notification
+            srg.addProgressListener(new ProgressListener() {
+                public void progressNotification(final ProgressEvent e) {
+                    notifyProgressListeners(e.getProgress(), e.getMessage());
+                }
+            });
+
+            // Run segmentation
+            srg.run();
+            final ByteProcessor r = srg.getRegionMarkers();
+            r.setColorModel(seeds.getCurrentColorModel());
+            return r;
+        }
+    }
 }
