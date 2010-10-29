65bcde5f1f23611eb94326e0339c9e8a4e5dcc2a
Jarek Sacha <jpsacha@gmail.com>
2010-10-29 01:33:59 +0000

    Add test for Bug-3097792.

--- test/src/net/sf/ij_plugins/io/vtk/VtkDecoderTest.java
+++ test/src/net/sf/ij_plugins/io/vtk/VtkDecoderTest.java
@@ -1,6 +1,7 @@
 /*
  * Image/J Plugins
- * Copyright (C) 2002-2008 Jarek Sacha
+ * Copyright (C) 2002-2010 Jarek Sacha
+ * Author's email: jsacha at users dot sourceforge dot net
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,32 +18,48 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Latest release available at http://sourceforge.net/projects/ij-plugins/
- *
  */
 package net.sf.ij_plugins.io.vtk;
 
 import ij.ImagePlus;
-import junit.framework.TestCase;
 import net.sf.ij_plugins.AssertUtils;
 import net.sf.ij_plugins.io.IOUtils;
+import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
 
 /**
  * @author Jarek Sacha
  * @since 11:05:51 PM  Nov 12, 2006
  */
-public final class VtkDecoderTest extends TestCase {
-    public VtkDecoderTest(String test) {
-        super(test);
-    }
+public final class VtkDecoderTest {
 
-    public void testBUG1594780() throws Exception {
-        final File extepectedImageFile = new File("test/data/BUG-1594780/test_COLOR_SCALARS_1.png");
+    @Test
+    public void testBug1594780() throws Exception {
+        final File expectedImageFile = new File("test/data/BUG-1594780/test_COLOR_SCALARS_1.png");
         final File actualImageFile = new File("test/data/BUG-1594780/test_COLOR_SCALARS_1.vtk");
 
-        assertTrue(extepectedImageFile.exists());
-        final ImagePlus expectedImp = IOUtils.openImage(extepectedImageFile.getAbsolutePath());
+        assertTrue(expectedImageFile.exists());
+        final ImagePlus expectedImp = IOUtils.openImage(expectedImageFile.getAbsolutePath());
+
+        assertTrue(actualImageFile.exists());
+        final ImagePlus actualImp = VtkDecoder.open(actualImageFile);
+
+        AssertUtils.equals(expectedImp, actualImp, 0);
+    }
+
+
+    @Test
+    public void testBug3097792() throws IOException, VtkImageException {
+        final File expectedImageFile = new File("test/data/VTK/lena.png");
+        final File actualImageFile = new File("test/data/VTK/lena_header_with_spaces.vtk");
+
+        assertTrue(expectedImageFile.exists());
+        final ImagePlus expectedImp = IOUtils.openImage(expectedImageFile.getAbsolutePath());
 
         assertTrue(actualImageFile.exists());
         final ImagePlus actualImp = VtkDecoder.open(actualImageFile);
