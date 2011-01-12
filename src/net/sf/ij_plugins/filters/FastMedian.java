376bbce26f0d180c1cd39c04036413b8ba781553
Jarek Sacha <jpsacha@gmail.com>
2011-01-12 01:01:44 +0000

    Add more javadoc.

--- src/net/sf/ij_plugins/filters/FastMedian.java
+++ src/net/sf/ij_plugins/filters/FastMedian.java
@@ -1,6 +1,7 @@
 /*
  * Image/J Plugins
- * Copyright (C) 2002-2008 Jarek Sacha
+ * Copyright (C) 2002-2011 Jarek Sacha
+ * Author's email: jsacha at users dot sourceforge dot net
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,7 +18,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Latest release available at http://sourceforge.net/projects/ij-plugins/
- *
  */
 package net.sf.ij_plugins.filters;
 
@@ -28,13 +28,31 @@ import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 import net.sf.ij_plugins.color.ColorProcessorUtils;
 
+
 /**
- * Helper for calling fast filter algorithms that match type of ImageProcessor.
+ * Helper for calling fast median filter algorithms.
+ * It will call an implementation that best matches input Image {@code ip}.
+ * <p/>
+ * Sample usa of a 7x7 median filter:
+ * <pre>
+ *   ColorProcessor src = ...
+ *   ColorProcessor dest = (ColorProcessor) FastMedian.process(src, 7);
+ * </pre>
  *
  * @author Jarek Sacha
  * @since Oct 23, 2008 10:01:46 PM
  */
 public final class FastMedian {
+
+    /**
+     * Apply median filter to input image {@code ip}.
+     * For color images ({@link ColorProcessor}) it will process each color band (R,G,B) independently.
+     *
+     * @param ip         input image.
+     * @param filterSize filter size (window size is filterSize x filterSize)
+     * @return median filtered input image. The type is the same sa input image,
+     *         so for ColorProcessor it will be ColorProcessor.
+     */
     public static ImageProcessor process(final ImageProcessor ip, final int filterSize) {
 
         final ImageProcessor dest;
@@ -65,6 +83,7 @@ public final class FastMedian {
 
         return dest;
     }
+
 
     private static ByteProcessor process(final ByteProcessor src, final int filterSize) {
         final FastMedianUInt8 filter = new FastMedianUInt8();
