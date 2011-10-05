dafb8ac867604211d081fc11001fde820ca41263
Jarek Sacha <jpsacha@gmail.com>
2011-10-05 02:09:57 +0000

    scheduled for removal

--- src/net/sf/ij_plugins/util/DialogUtil.java
+++ src/net/sf/ij_plugins/util/DialogUtil.java
@@ -1,6 +1,7 @@
 /*
  * Image/J Plugins
- * Copyright (C) 2002-2008 Jarek Sacha
+ * Copyright (C) 2002-2011 Jarek Sacha
+ * Author's email: jsacha at users dot sourceforge dot net
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,7 +18,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Latest release available at http://sourceforge.net/projects/ij-plugins/
- *
  */
 package net.sf.ij_plugins.util;
 
@@ -31,14 +31,18 @@ import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 
+
 /**
  * Utilities for simplifying creation of property editor dialogs.
  *
  * @author Jarek Sacha
+ * @deprecated scheduled for removal
  */
 public class DialogUtil {
+
     private DialogUtil() {
     }
+
 
     /**
      * Utility to automatically create ImageJ's GenericDialog for editing bean properties. It uses BeanInfo to extract
