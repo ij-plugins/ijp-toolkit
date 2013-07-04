97d11795617df1e2d2737317eabc50156815c8c4
Jarek Sacha <jpsacha@gmail.com>
2013-07-04 18:37:48 +0000

    Add more detailed tests for KMeans2D clustering.

--- test/src/net/sf/ij_plugins/clustering/KMeans2DTest.java
+++ test/src/net/sf/ij_plugins/clustering/KMeans2DTest.java
@@ -23,25 +23,85 @@
 package net.sf.ij_plugins.clustering;
 
 import ij.ImagePlus;
+import ij.ImageStack;
+import ij.process.ByteProcessor;
 import ij.process.ImageConverter;
 import ij.process.ImageProcessor;
 import ij.process.StackConverter;
 import net.sf.ij_plugins.io.IOUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 
 /**
+ * Unit tests for {@link net.sf.ij_plugins.clustering.KMeans2D}
+ *
  * @author Jarek Sacha
  */
 public final class KMeans2DTest {
 
+    @Test
+    public void test01() throws Exception {
+        final float tolerance = 0.001f;
+        final float[][] expectedCenters = {
+                {26.0207f, 50.91606f, 101.7027f},
+                {227.0155f, 30.7971f, 30.7971f},
+                {126.8642f, 0.0000f, 230.3005f},
+        };
+
+        // Read test image
+        final File imageFile = new File("test/data/Flamingo.png");
+        assertTrue("Input file should exist", imageFile.exists());
+        final ImagePlus imp = IOUtils.openImage(imageFile);
+        assertTrue("Expecting color image.", imp.getType() == ImagePlus.COLOR_RGB);
+
+        // Convert input color image to a floating point stack
+        new ImageConverter(imp).convertToRGBStack();
+        new StackConverter(imp).convertToGray32();
+        final ImageStack stack = imp.getStack();
+
+        // Setup m-means
+        final KMeansConfig config = new KMeansConfig();
+        config.setNumberOfClusters(expectedCenters.length);
+        config.setRandomizationSeedEnabled(true);
+        config.setRandomizationSeed(48);
+        final KMeans2D kmeans = new KMeans2D(config);
+
+        // Run k-means to produce cluster image
+        final ByteProcessor clusterImage = kmeans.run(stack);
+        assertNotNull(clusterImage);
+        assertEquals("Output width must match input width", imp.getWidth(), clusterImage.getWidth());
+        assertEquals("Output height must match input height", imp.getHeight(), clusterImage.getHeight());
+        final float[][] clusterCenters = kmeans.getClusterCenters();
+        assertNotNull(clusterCenters);
+        assertEquals(expectedCenters.length, clusterCenters.length);
+
+        // Since we are using a fixed randomization seed, the cluster centers should be predictable
+        for (int c = 0; c < expectedCenters.length; c++) {
+            assertArrayEquals(expectedCenters[c], clusterCenters[c], tolerance);
+        }
+
+        // Check cluster codes at some locations on the output clusterImage
+        assertEquals(0, clusterImage.getPixel(31, 160));
+        assertEquals(0, clusterImage.getPixel(252, 44));
+        assertEquals(0, clusterImage.getPixel(241, 238));
+        assertEquals(1, clusterImage.getPixel(95, 32));
+        assertEquals(1, clusterImage.getPixel(104, 129));
+        assertEquals(1, clusterImage.getPixel(241, 124));
+        assertEquals(2, clusterImage.getPixel(57, 81));
+        assertEquals(2, clusterImage.getPixel(176, 106));
+        assertEquals(2, clusterImage.getPixel(143, 277));
+    }
+
 
     @Test
-    public void test01() throws java.lang.Exception {
+    @Ignore("Only for benchmarking.")
+    public void benchmark01() throws IOException {
         final File imageFile = new File("test/data/clown24.png");
         final double tolerance = 0.01;
         final double[][] expectedCenters = {
@@ -83,11 +143,11 @@ public final class KMeans2DTest {
             assertNotNull(ip);
 
             float[][] centers = kmeans.getClusterCenters();
-//            for (int i = 0; i < centers.length; i++) {
-//                for (int j = 0; j < centers[i].length; j++) {
-//                    System.out.println("center[" + i + "][" + j + "]: " + centers[i][j]);
-//                }
-//            }
+            for (int i = 0; i < centers.length; i++) {
+                for (int j = 0; j < centers[i].length; j++) {
+                    System.out.println("center[" + i + "][" + j + "]: " + centers[i][j]);
+                }
+            }
 
             for (int i = 0; i < centers.length; i++) {
                 for (int j = 0; j < centers[i].length; j++) {
@@ -95,54 +155,14 @@ public final class KMeans2DTest {
                             expectedCenters[i][j], centers[i][j], tolerance);
                 }
             }
+            System.out.println("Steps: " + kmeans.getNumberOfStepsToConvergence());
         }
 
         System.out.println("Average time: " + totalTime / (double) iterations + "ms");
         System.out.println("Min time: " + minTime + "ms");
 
-//        System.out.println("Steps: " + kmeans.getNumberOfStepsToConvergence());
-
 //        final ImagePlus imp1 = new ImagePlus("K-means", ip);
 //        final FileSaver saver = new FileSaver(imp1);
 //        saver.saveAsTiff("kmeans-output.tif");
-    }
-
-
-    public void testColor() {
-//    ColorProcessor r = new ColorProcessor(stack.getWidth(), stack.getHeight());
-//
-//    // Encode output image
-//    byte[] pixels = new byte[bandSize];
-//    int[] x = new int[bands.length];
-//    int[] v = new int[3];
-//    int width = stack.getWidth();
-////    int height = stack.getHeight();
-//    for (int i = 0; i < pixels.length; i++) {
-//      for (int j = 0; j < bands.length; ++j) {
-//        x[j] = bands[j][i] & 0xff;
-//      }
-//      int c = closestCluster(x, clusterCenters);
-//      pixels[i] = (byte) (c & 0xff);
-//
-//      v[0] = (int) java.lang.Math.round(clusterCenters[c][0]);
-//      v[1] = (int) java.lang.Math.round(clusterCenters[c][1]);
-//      v[2] = (int) java.lang.Math.round(clusterCenters[c][2]);
-//      int yy = i / width;
-//      int xx = i % width;
-//      r.putPixel(xx, yy, v);
-//    }
-//
-//    java.lang.System.out.println("Cluster centers");
-//    for (int i = 0; i < clusterCenters.length; i++) {
-//      double[] clusterCenter = clusterCenters[i];
-//      java.lang.System.out.print("(");
-//      for (int j = 0; j < clusterCenter.length; j++) {
-//        double vv = clusterCenter[j];
-//        java.lang.System.out.print(" " + vv + " ");
-//      }
-//      java.lang.System.out.println(")");
-//    }
-//
-//
     }
 }
